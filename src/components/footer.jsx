604a48206fc51d88ddeab3270e95a2fa2e658df1
lysa.dev <Lysauniverse30@gmail.com>
2025-05-03 14:18:58 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,15 +8,15 @@ function Footer() {
                 <p>>2025 LYΣA.</p>
             <div className="social-icons">
                     <p>
-                        <a href="https://x.com/xai" target="_blank" rel="noopener noreferrer">
+                        <a href="https://x.com/exe_01ySA?t=_TzMX5dPmWHwtMUcFDkmcA&s=09" target="_blank" rel="noopener noreferrer">
                             <FaXTwitter />
                         </a>{' '}
                         |{' '}
-                        <a href="https://x.com/xai" target="_blank" rel="noopener noreferrer">
+                        <a href="https://t.me/exe_01ySA" target="_blank" rel="noopener noreferrer">
                             <FaTelegram />
                         </a>{' '}
                         |{' '}
-                        <a href="https://x.com/xai" target="_blank" rel="noopener noreferrer">
+                        <a href="https://github.com/LY-S-A" target="_blank" rel="noopener noreferrer">
                             <FaGithub />
                         </a>
                     </p>
