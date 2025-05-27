821aef0bed8682f7549d81827a8745b71f67fd39
lysa.waitlist <Lysauniverse30@gmail.com>
2025-05-27 08:59:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+// import Header from './components/header';
+// import Hero from './components/hero';
+// import About from './components/about';
+// import Features from './components/features';
+// import CopyCa from './components/ca';
+// import Contact from './components/contact';
+// import Footer from './components/footer';
+// import './App.css';
+
+// function App() {
+//   return (
+//       <div className="App">
+//         <Header />
+//                 <Hero />
+//                 <About />
+//                 <Features />
+//                 <CopyCa />
+//                 <Contact />
+//                 <Footer />
+//       </div>
+//   );
+// }
+
+// export default App;
+
 import Header from './components/header';
 import Hero from './components/hero';
 import About from './components/about';
@@ -5,20 +30,20 @@ import Features from './components/features';
 import CopyCa from './components/ca';
 import Contact from './components/contact';
 import Footer from './components/footer';
-import './App.css';
-
-function App() {
+import Waitlist from './components/waitlist';
+import './App.css';function App() {
   return (
-      <div className="App">
-        <Header />
-                <Hero />
-                <About />
-                <Features />
-                <CopyCa />
-                <Contact />
-                <Footer />
-      </div>
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Features/>
+      <CopyCa/>
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
