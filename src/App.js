ee53c70f8ddc97d35de0e7734f5329db630801ab
lysa.waitlist <Lysauniverse30@gmail.com>
2025-05-27 08:53:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import Header from './components/header';
-// import Hero from './components/hero';
-// import About from './components/about';
-// import Features from './components/features';
-// import CopyCa from './components/ca';
-// import Contact from './components/contact';
-// import Footer from './components/footer';
-// import Waitlist from './components/waitlist';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header />
-//       <Hero />
-//       <About />
-//       <Features/>
-//       <CopyCa/>
-//       <Contact />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Hero from './components/hero';
