c74dfa1168d613071c0bed70fcb2a82df359aac4
lysa.waitlist <Lysauniverse30@gmail.com>
2025-05-27 08:55:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Hero from './components/hero';
 import About from './components/about';
@@ -6,7 +5,6 @@ import Features from './components/features';
 import CopyCa from './components/ca';
 import Contact from './components/contact';
 import Footer from './components/footer';
-import Waitlist from './components/waitlist';
 import './App.css';
 
 function App() {
@@ -14,21 +12,12 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
                 <Hero />
                 <About />
                 <Features />
                 <CopyCa />
                 <Contact />
                 <Footer />
-              </>
-            }
-          />
-          <Route path="/waitlist" element={<Waitlist />} />
         </Routes>
       </div>
     </Router>
