803a1791932ae53924e27f41fdb173054710d45a
lysa.waitlist <Lysauniverse30@gmail.com>
2025-05-27 08:14:30 +0100

    Update waitlist.jsx

--- src/components/waitlist.jsx
+++ src/components/waitlist.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FaXTwitter, FaGithub, FaTelegram } from 'react-icons/fa6';
-import './waitlist.css';
+import '../styles/waitlist.css';
 
 const LoadingBar = ({ progress }) => {
   return (
