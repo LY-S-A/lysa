1c0c471efdc683297aa86b8c974750689e7f886a
lysa.waitlist <Lysauniverse30@gmail.com>
2025-05-31 18:52:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import Header from './components/header';
-// import Hero from './components/hero';
-// import About from './components/about';
-// import Features from './components/features';
-// import CopyCa from './components/ca';
-// import Contact from './components/contact';
-// import Footer from './components/footer';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header />
-//       <Hero />
-//       <About />
-//       <Features/>
-//       <CopyCa/>
-//       <Contact />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Hero from './components/hero';
@@ -49,12 +24,13 @@ function App() {
                 <Features />
                 <CopyCa />
                 <Contact />
+                <Footer />
               </>
             }
           />
           <Route path="/glitch" element={<ImageGlitchUploader />} />
         </Routes>
-        <Footer />
+        
       </div>
     </Router>
   );
