3d353b3c7331e1d15c32ec5920bb4354c77bcdfa
lysa.waitlist <Lysauniverse30@gmail.com>
2025-06-02 14:48:07 +0100

    Create leaderboard.jsx

---
+++ src/components/leaderboard.jsx
@@ -0,0 +1,18 @@
+// ./components/leaderboard.jsx
+import React from 'react';
+
+
+  return (
+    <section className="leaderboard">
+      <div className="container">
+        <h2>Leaderboard</h2>
+        <p>Big shoutout to our top contributors who went above and beyond for the project! 
+ Your hard work, ideas, and dedication have not gone unnoticed — it’s time to celebrate with rewards! </p>
+        
+        <button>View Leaderboard</button>
+      </div>
+    </section>
+  );
+}
+
+export default Leaderboard;
