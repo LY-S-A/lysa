3c2d86639b50069e2335a4dbf77262595c1fdeb2
lysa.waitlist <Lysauniverse30@gmail.com>
2025-06-03 12:16:24 +0100

    Update leaderboard.jsx

--- src/components/leaderboard.jsx
+++ src/components/leaderboard.jsx
@@ -1,7 +1,16 @@
 // ./components/leaderboard.jsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Leaderboard () {
+
+   const navigate = useNavigate();
+    
+    const handleClick = () => {
+        navigate('/main-leaderboard');
+    };
+
+  
   return (
     <section className="leaderboard">
       <div className="container">
@@ -9,7 +18,9 @@ function Leaderboard () {
         <p>Big shoutout to our top contributors who went above and beyond for the project! 
  Your hard work, ideas, and dedication have not gone unnoticed — it’s time to celebrate with rewards! </p>
         
-        <button>View Leaderboard</button>
+        <button
+         onClick={handleClick} 
+          >View Leaderboard</button>
       </div>
     </section>
   );
